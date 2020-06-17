17d6493a3c2e5a470bd0ee9b029edf2ed919fdbb
Ivan <65360117+ivanss26@users.noreply.github.com>
2020-06-17 05:22:42 -0300

    Add files via upload

---
+++ estrutura-para-faca.js
@@ -0,0 +1,29 @@
+/* 
+Var
+// Seção de Declarações das variáveis 
+   numero, fatorial, contador: inteiro
+   
+
+Inicio
+// Seção de Comandos, procedimento, funções, operadores, etc... 
+   escreva("digite o numero para calcular o Fatorial.")
+   leia (numero)
+   fatorial := 1
+   para contador de 1 ate numero faca
+        fatorial := fatorial * contador
+   fimpara
+   
+   escreva("O fatorial de ", numero, " é", fatorial)
+   */
+
+  function acaoBotao() {
+    var numero, fatorial
+    numero = prompt("digite o numero para calcular o Fatorial.")
+    fatorial = 1
+    for (var contador = 1; contador <= numero; contador++) {
+        fatorial = fatorial * contador
+        
+    }
+
+    document.getElementById("paragrafo").innerText = "O fatorial de "+ numero+ " é:"+ fatorial
+}
